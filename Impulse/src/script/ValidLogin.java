22beafa874b67b5ecde461403eb7383cc31c38e5
Ambika <Ambika@DESKTOP-6HUU505>
2019-03-02 16:39:24 +0530

    Modifications

--- Impulse/src/script/ValidLogin.java
+++ Impulse/src/script/ValidLogin.java
@@ -1,8 +1,12 @@
 package script;
 
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 @Test
 public class ValidLogin {
 
+	public void test() {
+		Reporter.log("testing github");
+	}
 }
